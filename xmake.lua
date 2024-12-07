f9efb2017d8b5ee4a5ab0f2e36e4439e5e2f857c
LYJ <532874387@qq.com>
2024-12-07 19:06:20 +0800

    使用xmake 构建示例

---
+++ xmake.lua
@@ -0,0 +1,20 @@
+add_rules("mode.debug", "mode.release")
+
+set_languages("c++23", "c17")--设置编译语言
+
+set_encodings("utf-8")--设源文件编码和可执行文件输出编码 utf-8
+
+set_project("raylib-cpp-cn")-- 设工程名
+
+set_version("0.0.1")-- 设工程版本
+
+add_requires("raylib 5.5") --添加依赖包
+
+add_packages("raylib")
+
+add_includedirs("include",{public = true}) --添加头文件搜索路径,公开
+ 
+
+includes("**/xmake.lua")--搜索目录下所有子构建
+
+add_defines("ROOT")--添加宏定义
